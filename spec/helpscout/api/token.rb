a2500f59a14b7a5250848268bd8c29d32ab009b9
Jason Jacob <jj@Jasons-MBP.lan>
2020-03-04 19:34:03 +1030

    remove unused file

--- spec/helpscout/api/token.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Helpscout::Api::Auth::Token do
-  it 'creates a token class' do
-    expect(Helpscout::Api::Auth::Token.new).not_to be nil
-  end
-end
